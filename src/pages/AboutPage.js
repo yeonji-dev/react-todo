2461f4339c086c50e82a6188797ef4a4d10b6fb5
yeonji-dev <lilianaaa804@gmail.com>
2023-12-01 16:27:49 +0900

    Config: layout - children

--- src/pages/AboutPage.js
+++ src/pages/AboutPage.js
@@ -1,6 +1,9 @@
+import BasicLayout from "../layouts/BasicLayout";
 const AboutPage = () => {
     return (
-        <div className="text-3xl">About Page</div>
+        <BasicLayout>
+            <div className="text-3xl">About Page</div>
+        </BasicLayout>
     );
 }
 
--- src/pages/MainPage.js
+++ src/pages/MainPage.js
@@ -1,13 +1,11 @@
 import {Link} from "react-router-dom";
+import BasicLayout from "../layouts/BasicLayout";
 
 const MainPage = () => {
     return (
-        <div>
-            <div className="flex">
-                <Link to={'/about'}>About</Link>
-            </div>
+        <BasicLayout>
             <div className="text-3xl">Main Page</div>
-        </div>
+        </BasicLayout>
     );
 }
 
