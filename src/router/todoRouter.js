333a49ff8ba48dd6165c31820509cab5d6ec9f12
yeonji-dev <lilianaaa804@gmail.com>
2023-12-05 11:39:11 +0900

    Config: URL Param 사용 - AddPage, ModifyPage, ReadPage

--- src/router/todoRouter.js
+++ src/router/todoRouter.js
@@ -3,6 +3,9 @@ import {Navigate} from "react-router-dom";
 
 const Loading = <div>Loading...</div>
 const TodoList = lazy(() => import("../pages/todo/ListPage"))
+const TodoRead = lazy(() => import("../pages/todo/ReadPage"))
+const TodoAdd = lazy(() => import("../pages/todo/AddPage"))
+const TodoModify = lazy(() => import("../pages/todo/ModifyPage"))
 
 const todoRouter = () => {
     return [
@@ -14,6 +17,18 @@ const todoRouter = () => {
             path: "",
             element: <Navigate replace to="list"></Navigate>
         },
+        {
+            path: "read/:tno",
+            element: <Suspense fallback={Loading}><TodoRead/></Suspense>
+        },
+        {
+            path: "add",
+            element: <Suspense fallback={Loading}><TodoAdd/></Suspense>
+        },
+        {
+            path: "modify/:tno",
+            element: <Suspense fallback={Loading}><TodoModify/></Suspense>
+        }
     ]
 }
 
